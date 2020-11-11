eb6c4047f233ffc512c985bc781fb4bc99db63c3
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-11 14:33:12 -0800

    Creating scatterplot for length vs. scalelength, and colored points with lakeid

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -1,16 +1,29 @@
-# First Commits ----
+# Exercise 1 - First Commits ----
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.1.csv")
-
-# Building a History
 library(dplyr)
+library(ggplot2)
+
+
+#Exercise 2 - First Solo Commit ----
 #creating column classifying fish bigger than 300mm as big (others as small)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+
 
 # Change Category cut-off size
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
 
-# Second Solo Commit ----
+
+# Exercise 3 - Second Solo Commit ----
 #scale length > 1 mm
 fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
+
+
+# Exercise 4 - Commit Multiple Files ----
+#Changed file name from Gaeta_etal_CLC_data.csv to Gaeta_etal_CLC_data.1.csv
+
+#Exercise 5 - Pushing Changes ----
+ggplot(fish_data_cat, aes(x = length, y = scalelength, color = lakeid)) +
+  geom_point() +
+  labs( x = "Length", y = "Scalelength")
