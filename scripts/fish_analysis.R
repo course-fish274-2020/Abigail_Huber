87f44cc60813b695cc6c9c81bc3f85e486d7f1a3
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-11 13:57:48 -0800

    Filtered out fish scalelength greater than 1mm

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -1,7 +1,7 @@
 # First Commits ----
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.1.csv")
 
-# Building a History ----
+# Building a History
 library(dplyr)
 #creating column classifying fish bigger than 300mm as big (others as small)
 fish_data_cat <- fish_data %>%
@@ -10,3 +10,7 @@ fish_data_cat <- fish_data %>%
 # Change Category cut-off size
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+# Second Solo Commit ----
+#scale length > 1 mm
+fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
