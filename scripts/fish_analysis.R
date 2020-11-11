98578dece42c8448fce55dc4c1a7dde4ca22a462
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-11 15:23:29 -0800

    Saving ggplot for Exercise 6

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -8,8 +8,6 @@ library(ggplot2)
 #creating column classifying fish bigger than 300mm as big (others as small)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
-
-
 # Change Category cut-off size
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 300, "big", "small"))
@@ -23,12 +21,18 @@ fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
 # Exercise 4 - Commit Multiple Files ----
 #Changed file name from Gaeta_etal_CLC_data.csv to Gaeta_etal_CLC_data.1.csv
 
+
 #Exercise 5 - Pushing Changes ----
 ggplot(fish_data_cat, aes(x = length, y = scalelength, color = lakeid)) +
   geom_point() +
   labs( x = "Length", y = "Scalelength")
-
-
 #Instructor comment
 ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram()
+
+
+#Exercise 6 - Pulling and Pushing
+# Modifying code above to look at narrower ranges by changing bins and argument
+ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram(bins = 80) +
+ggsave("scale_hist_by_length.jpg")
