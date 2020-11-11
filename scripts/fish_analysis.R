03133f4d7cd272e76d5e25f6b128446582cd4358
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-11 12:10:18 -0800

    Comment for creating column that classifies fish size as "big" and "small"

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -3,6 +3,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.1.csv")
 
 # Building a History ----
 library(dplyr)
+#creating column classifying fish bigger than 300mm as big (others as small)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
 
