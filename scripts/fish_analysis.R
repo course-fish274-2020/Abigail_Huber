0cc5b90e73575e41fb76fd699bc493e5461a01e8
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-11 11:42:48 -0800

    Add fish size and growth rate data Start script comparing fish length and scale size

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -1,21 +1,3 @@
 #reading in data ----
 fish_data <- read.csv("data/Gaeta_etal_CLC_data.1.csv")
 
-library(dplyr)
-fish_data_cat <- fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small"))
-
-
-# creating an additional column that classifies fish bigger than 300mm as big (others as small)
-
-fish_data_cat <- fish_data %>%
-  mutate(length_cat = ifelse(length > 300, "big", "small"))
-
-#scale length > 1 mm
-fish_data_cat_clean <- filter(fish_data_cat, scalelength > 1)
-
-
-
-ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
-  geom_histogram()
-
